cd96abc8a9f4b2e0aff711f6482efe845a3173ed
vijaymadh <vijaymadh@gmail.com>
2025-03-09 16:12:04 +0530

    Commit Changes
    Commit Changes

---
+++ LeaningApp/LeaningApp/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaningApp
+{
+    class BubbleSort
+    {
+        static BubbleSort _bubbleSort;
+        BubbleSort()
+        {
+
+        }
+        public static BubbleSort GetInstance()
+        {
+            _bubbleSort = _bubbleSort == null ? new BubbleSort() : _bubbleSort;
+            return _bubbleSort;
+        }
+
+        internal void SortArray()
+        {
+            int[] oldArray = {3,6,4,7,9,11,5,12,13 };
+            int[] sortedArray = { };
+            var tempStore=0;
+
+            for (int i = 0; i <= oldArray.Length - 1; i++)
+            {
+                for (int j = 0; j <= oldArray.Length - 2; j++)
+                {
+                    if (oldArray[j] > oldArray[j + 1])
+                    {
+                        tempStore = oldArray[j + 1];
+                        oldArray[j + 1] = oldArray[j];
+                        oldArray[j] = tempStore;
+
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(",",oldArray));
+
+        }
+    }
+}
---
+++ LeaningApp/LeaningApp/Factorial.cs
@@ -0,0 +1,28 @@
+﻿namespace LeaningApp
+{
+    class Factorial
+    {
+        static Factorial _factorial;
+        Factorial()
+        {
+
+        }
+        public static Factorial GetInstance()
+        {
+            _factorial = _factorial == null ? new Factorial() : _factorial;
+            return _factorial;
+        }
+
+        internal void PrintFactorial(int value)
+        {
+            var factotial = 1;
+            for (int i = 1; i <= value; i++)
+            {
+                factotial = factotial * i;
+            }
+
+            Console.WriteLine($"Factorial is {factotial}");
+
+        }
+    }
+}
